1561de9e5432d7d0edcd68162dedc72081d11567
Trisha <magallentrisha816@gmail.com>
2021-04-17 11:20:00 +0800

    Fix memory exhausting during baranggay seeding

--- database/seeds/PhilippineBaranggaySeeder.php
+++ database/seeds/PhilippineBaranggaySeeder.php
@@ -12,8 +12,18 @@ class PhilippineBaranggaySeeder extends Seeder
      */
     public function run()
     {
-        if(!DB::table('philippine_baranggays')->count()) {
+        /*if(!DB::table('philippine_baranggays')->count()) {
             DB::unprepared(file_get_contents(__DIR__ . '/sql/philippine_barangays.sql'));
-        }
+        }*/
+        $database = config('database.connections.mysql.database');
+        $user = config('database.connections.mysql.username');
+        $password = config('database.connections.mysql.password');
+
+        /*$this->command->info($database);
+        $this->command->info($user);
+        $this->command->info($password);*/
+
+        // running command line import in php code
+        exec('mysql -u '.$user.' -p '.$password.' '.$database.' < ./database/seeds/sql/philippine_barangays.sql');
     }
 }
