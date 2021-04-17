59d81629042c1e61ea636b4b59be506f59b1dea8
Trisha <magallentrisha816@gmail.com>
2021-04-17 13:56:47 +0800

    Configure filesystems.php

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -7,7 +7,15 @@
 
 <section class="relative container mx-auto px-4 -mt-80 mb-10">
 	<div class="p-4 bg-white shadow-md rounded-lg">
-		
+		<div class="flex flex-col">
+			<div class="flex">
+				<img src="">
+			</div>
+
+			<div>
+				
+			</div>
+		</div>
 	</div>
 </section>
 @endsection
